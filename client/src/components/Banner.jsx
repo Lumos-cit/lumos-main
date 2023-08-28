885efa348255808c2b34551b7dcfeceb1131e7a4
Surieya <surieyasurieya@gmail.com>
2023-08-28 16:34:42 +0530

    banner-changes

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -9,35 +9,35 @@ const Banner = ({ setShowBanner }) => {
         // style={{ backgroundImage: `url(${newBanner})` }}
       >
         <img className="w-[100%] h-[100%]" src={newBanner} alt="" />
-        <div className="hidden md:flex gap-12 pb-3 w-full justify-center">
-          <button
+        <div className=" flex md:flex gap-12 pb-3 w-full justify-center">
+          {/* <button
             className="text-[10px] md:text-[12px] px-2 md:px-5 py-1 font-bold bg-slate-100 text-slate-950 rounded-md hover:bg-slate-900 hover:text-slate-100"
             onClick={() => setShowBanner(false)}
           >
             CLOSE
-          </button>
+          </button> */}
           <a
-            className="text-[10px] md:text-[12px] px-5 py-1 font-bold  bg-slate-100 text-slate-950 rounded-md hover:bg-slate-900 hover:text-slate-100"
+            className="text-[12px] md:text-[14px] px-9 py-1 font-bold  bg-slate-100 text-slate-950 rounded-md hover:bg-slate-900 hover:text-slate-100"
             href="https://hnm.lumoscit.in"
             target="_blank"
           >
-            EXPLORE NOW
+            EXPLORE
           </a>
         </div>
       </div>
       <button
-        className="text-white text-[20px] font-bold absolute top-6 right-10"
+        className="text-white text-[26px] font-bold absolute top-6 right-10"
         onClick={() => setShowBanner(false)}
       >
         X
       </button>
-      <a
-        className="text-[10px] md:text-[12px] px-5 py-1 font-bold  bg-slate-100 text-slate-950 rounded-md hover:bg-slate-900 hover:text-slate-100 absolute top-7 left-6 md:hidden"
+      {/* <a
+        className="text-[10px] md:text-[12px] px-5 py-1 font-bold  bg-slate-100 text-slate-950 rounded-md hover:bg-slate-900 hover:text-slate-100 absolute top-8 left-6 md:hidden"
         href="https://hnm.lumoscit.in"
         target="_blank"
       >
-        EXPLORE NOW
-      </a>
+        EXPLORE
+      </a> */}
     </main>
   );
 };
